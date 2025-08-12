595bee277120ec2a03f0cd7db8452adc1275ac92
JOHN DENVER CAMACHO JAMILI <johndenverjamili@gmail.com>
2025-08-12 17:45:30 +0800

    Add files via upload

---
+++ postNget.php
@@ -0,0 +1,13 @@
+// GET example: get user by ID
+<form method="GET">
+  <input type="text" name="id">
+    <input type="password" name="password">
+  <button type="submit">Search</button>
+</form>
+
+// POST example: submit login form
+<form method="POST">
+  <input type="text" name="username">
+  <input type="password" name="password">
+  <button type="submit">Login</button>
+</form>
