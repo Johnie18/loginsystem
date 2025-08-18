e93a1a39edf5d2f3764455de12e90f1bb2fc50d2
JOHN DENVER CAMACHO JAMILI <johndenverjamili@gmail.com>
2025-08-18 20:39:05 +0800

    Deleting Files
    This files is not connected with the login system project so I decided to delete it and move it to another repository

--- function.php
+++
@@ -1,9 +0,0 @@
-<?php
- function happybirthday($name, $age){
-
-    echo "HAPPY BIRTHDAY{$name} ! <br>";
-    echo "YOU'RE NOW {$age} !<br><br>";
- }
- happybirthday("JOHN", 23);
-  happybirthday("MELODY", 22);
-?>
